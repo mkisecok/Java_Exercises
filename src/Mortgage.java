07625c8105a7932ac5d285058ccc156873cf1e0b
Mkisecok <mahmutkisecok@gmail.com>
2022-04-14 13:19:31 +0200

    docs: Delete Main class

--- src/Mortgage.java
+++ src/Mortgage.java
@@ -5,13 +5,13 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 
-public class Main {
+public class Mortgage {
     
     public static void main(String[] args) {
     
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
-        
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Principal: ");
