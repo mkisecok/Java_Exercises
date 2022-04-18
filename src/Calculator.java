6b3f509826385b1b73c715f89515bf67a1d57d3d
Mkisecok <mahmutkisecok@gmail.com>
2022-04-18 14:03:28 +0200

    fixed: Modified case4 for the condition of number2 is equal to 0

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number =scanner.nextInt();
+
+        int number1, number2 = 0;
+        System.out.print("Enter a first number: ");
+         number1 = scanner.nextInt();
+
         System.out.print("Enter a transaction number for: \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide \n 5. Modulus \n 6. Power \n 7. Square Root \n 8. Exit \n");
         int transaction = scanner.nextInt();
-        int number2=0;
+
         if(transaction != 7 && transaction != 8) {
             System.out.print("Enter a second number:");
             number2 = scanner.nextInt();
@@ -17,25 +20,29 @@ public class Calculator {
         
         switch (transaction) {
             case 1:
-                System.out.println(number + number2);
+                System.out.println(number1 + number2);
                 break;
             case 2:
-                System.out.println(number - number2);
+                System.out.println(number1 - number2);
                 break;
             case 3:
-                System.out.println(number * number2);
+                System.out.println(number1 * number2);
                 break;
             case 4:
-                System.out.println(number / number2);
+                if(number2 == 0) {
+                    System.out.println("Division by zero is not allowed");
+                } else {
+                    System.out.println(number1 / number2);
+                }
                 break;
             case 5:
-                System.out.println(number % number2);
+                System.out.println(number1 % number2);
                 break;
             case 6:
-                System.out.println(Math.pow(number, number2));
+                System.out.println(Math.pow(number1, number2));
                 break;
             case 7:
-                System.out.println(Math.sqrt(number));
+                System.out.println(Math.sqrt(number1));
                 break;
             case 8:
                 System.out.println("Exiting");
